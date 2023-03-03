6deba9dbef3f218f3ea91c5abea51b629b9e3f85
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-03-03 16:37:19 +0530

    updated linkedin link

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/shubham-deulkar-4b3231139/",
       style: "rounded-tr-md",
     },
     {
